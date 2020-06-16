9d00ab72483c4a57a24811a0d0d4aed3cc7ea254
avnoto <alexandravnoto@gmail.com>
2020-06-16 14:22:36 -0700

    app.js made

--- Develop/app.js
+++ Develop/app.js
@@ -10,8 +10,6 @@ const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 const render = require('./lib/htmlRenderer');
 
-//Ryan Skog is awesome
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
@@ -34,3 +32,132 @@ const render = require('./lib/htmlRenderer');
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+const teamMembers = [];
+
+function generateHTML() {
+  fs.writeFileSync(outputPath, render(teamMembers));
+}
+
+const questions = [
+  {
+    type: 'input',
+    message: "What is your manager's name?",
+    name: 'managerName',
+  },
+  {
+    type: 'input',
+    message: "What is your manager's ID?",
+    name: 'managerID',
+  },
+  {
+    type: 'input',
+    message: "What is your manager's email?",
+    name: 'managerEmail',
+  },
+  {
+    type: 'input',
+    message: "What is your manager's office Number?",
+    name: 'managerOfficeNumber',
+  },
+  {
+    type: 'input',
+    message: "What is your intern's name?",
+    name: 'internName',
+  },
+  {
+    type: 'input',
+    message: "What is your intern's ID?",
+    name: 'internID',
+  },
+  {
+    type: 'input',
+    message: "What is your intern's email?",
+    name: 'internEmail',
+  },
+  {
+    type: 'input',
+    message: "What is your intern's school?",
+    name: 'internSchool',
+  },
+  {
+    type: 'input',
+    message: "What is your Engineer's name?",
+    name: 'engineerName',
+  },
+  {
+    type: 'input',
+    message: "What is your Engineer's ID?",
+    name: 'engineerID',
+  },
+  {
+    type: 'input',
+    message: "What is your Engineer's email?",
+    name: 'engineerEmail',
+  },
+  {
+    type: 'input',
+    message: "What is your Engineer's Github?",
+    name: 'engineerGithub',
+  },
+];
+
+function createManager() {
+  const manager = new Manager(
+    inquirer.prompt(questions).then((res) => {
+      res.managerName, res.managerID, res.managerEmail, res.managerOfficeNumber;
+    })
+  );
+  teamMembers.push(manager);
+
+  createTeam();
+}
+function createIntern() {
+  const intern = new Intern(
+    inquirer.prompt(questions).then((res) => {
+      res.internName, res.internID, res.internEmail, res.internSchool;
+    })
+  );
+  teamMembers.push(intern);
+
+  createTeam();
+}
+function createEngineer() {
+  const engineer = new Engineer(
+    inquirer.prompt(questions).then((res) => {
+      res.engineerName, res.engineerID, res.engineerEmail, res.engineerGithub;
+    })
+  );
+  teamMembers.push(engineer);
+
+  createTeam();
+}
+
+function createTeam() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        choices: ['Engineer', 'Intern', 'Done'],
+        message: 'Which team member would you like to add?',
+        name: 'teamChoice',
+      },
+    ])
+    .then(({ teamChoice }) => {
+      if (teamChoice === 'Engineer') {
+        createEngineer();
+      }
+      if (teamChoice === 'Intern') {
+        createIntern();
+      }
+      if (teamChoice === 'Done') {
+        generateHTML();
+      }
+    });
+}
+
+function init() {
+  createManager();
+}
+
+init();
