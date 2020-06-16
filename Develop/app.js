f59e1e60f0b1f3ae9c48ef4151325466dc29ce6a
avnoto <alexandravnoto@gmail.com>
2020-06-16 16:55:40 -0700

    finishing readme, generateHTML successful, passes all tests

--- Develop/app.js
+++ Develop/app.js
@@ -10,36 +10,13 @@ const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
 const render = require('./lib/htmlRenderer');
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
 const teamMembers = [];
 
 function generateHTML() {
   fs.writeFileSync(outputPath, render(teamMembers));
 }
 
-const questions = [
+const managerQuestions = [
   {
     type: 'input',
     message: "What is your manager's name?",
@@ -60,6 +37,9 @@ const questions = [
     message: "What is your manager's office Number?",
     name: 'managerOfficeNumber',
   },
+];
+
+const internQuestions = [
   {
     type: 'input',
     message: "What is your intern's name?",
@@ -80,6 +60,9 @@ const questions = [
     message: "What is your intern's school?",
     name: 'internSchool',
   },
+];
+
+const engineerQuestions = [
   {
     type: 'input',
     message: "What is your Engineer's name?",
@@ -103,34 +86,40 @@ const questions = [
 ];
 
 function createManager() {
-  const manager = new Manager(
-    inquirer.prompt(questions).then((res) => {
-      res.managerName, res.managerID, res.managerEmail, res.managerOfficeNumber;
-    })
-  );
-  teamMembers.push(manager);
-
-  createTeam();
+  inquirer.prompt(managerQuestions).then((res) => {
+    const manager = new Manager(
+      res.managerName,
+      res.managerID,
+      res.managerEmail,
+      res.managerOfficeNumber
+    );
+    teamMembers.push(manager);
+    createTeam();
+  });
 }
 function createIntern() {
-  const intern = new Intern(
-    inquirer.prompt(questions).then((res) => {
-      res.internName, res.internID, res.internEmail, res.internSchool;
-    })
-  );
-  teamMembers.push(intern);
-
-  createTeam();
+  inquirer.prompt(internQuestions).then((res) => {
+    const intern = new Intern(
+      res.internName,
+      res.internID,
+      res.internEmail,
+      res.internSchool
+    );
+    teamMembers.push(intern);
+    createTeam();
+  });
 }
 function createEngineer() {
-  const engineer = new Engineer(
-    inquirer.prompt(questions).then((res) => {
-      res.engineerName, res.engineerID, res.engineerEmail, res.engineerGithub;
-    })
-  );
-  teamMembers.push(engineer);
-
-  createTeam();
+  inquirer.prompt(engineerQuestions).then((res) => {
+    const engineer = new Engineer(
+      res.engineerName,
+      res.engineerID,
+      res.engineerEmail,
+      res.engineerGithub
+    );
+    teamMembers.push(engineer);
+    createTeam();
+  });
 }
 
 function createTeam() {
--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# Team-Profile-Generator
+# Team Profile Generator
+
+A CLI Application that builds a software engineering team. Created using HTML, Bootstrap, Javascript, and Node.js
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Roadmap](#roadmap)
+- [Support](#support)
+- [Acknowledgements](#acknowledgements)
+
+## Installation
+
+1. Clone the repo
+
+```sh
+git clone https://github.com/avnoto/Team-Profile-Generator.git
+```
+
+2. Open the terminal in the application folder
+
+3. Install NPM Packages
+
+```sh
+npm i
+```
+
+4. Run the application
+
+```sh
+node app
+```
+
+## Usage
+
+[Inquirer npm package](https://github.com/SBoudrias/Inquirer.js/) is used to prompt the user for information about the team manager and then information about the team members. The user can input any number of team members, and they may be a mix of engineers and interns. When the user has completed building the team, the application will create an HTML file that displays a nicely formatted team roster based on the information provided by the user.
+
+## Roadmap
+
+I would like to work on more frontend development in the future for this project, in regards to the design of team.html.
+
+## Support
+
+Please [open an issue](https://github.com/avnoto/Team-Profile-Generator/issues/new) for support.
+
+Contact me at alexandravnoto@gmail.com with any questions or concerns.
+
+## Acknowledgements
+
+Justin Singh provided assistance with a video to the solution.
